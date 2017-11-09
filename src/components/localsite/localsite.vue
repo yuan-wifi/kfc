a347c2f1666879a580b50c5c8f6ada64be365694
yuan-wifi <1372284129@qq.com>
2017-11-09 16:49:42 +0800

    封装amap

--- src/components/localsite/localsite.vue
+++ src/components/localsite/localsite.vue
@@ -1,37 +1,26 @@
 <template>
   <div class="localsite">
+    <h3 class="title">{{ msg }}</h3>
+    <div class="amap-wrapper">
+      <el-amap class="amap-box" :vid="'amap-vue'"></el-amap>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'localsite',
-    mounted () {
-      let options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-      }
-      this.$nextTick(() => {
-        navigator.geolocation.getCurrentPosition(this.success, this.error, options)
-      })
-    },
-    methods: {
-      success (pos) {
-        let crd = pos.coords
-
-        console.log('Your current position is:')
-        alert('Latitude : ' + crd.latitude)
-        console.log('Longitude: ' + crd.longitude)
-        console.log('More or less ' + crd.accuracy + ' meters.')
-      },
-      error (err) {
-        console.warn('ERROR(' + err.code + '): ' + err.message)
-      }
+export default {
+  name: 'localsite',
+  data () {
+    return {
+      msg: 'vue-amap向你问好！'
     }
   }
+}
 </script>
 
-<style lang="stylus" ref="stylesheet/stylus">
-
+<style>
+.amap-box {
+  width: 500px;
+  height: 500px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import './common/stylus/index.styl'
+import AMap from 'vue-amap'
 
 Vue.config.productionTip = false
+
+Vue.use(AMap)
+AMap.initAMapApiLoader({
+  key: 'a333e15f5c979e23cd12963d2a0343c3',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+  uiVersion: '1.0.11' // 版本号
+})
 
 /* eslint-disable no-new */
 new Vue({
